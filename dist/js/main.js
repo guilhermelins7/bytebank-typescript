02fd4777847115a7d36bf0c72e153e7441912f9f
guilhermelins7 <guilherme7lins@gmail.com>
2025-03-04 18:07:01 -0300

    fix: adicionar dist/

---
+++ dist/js/main.js
@@ -0,0 +1,4 @@
+// Importando módulos:
+import "./components/nova-transacao-component.js";
+import "./components/saldo-component.js";
+import "./components/extrato-component.js";
---
+++ dist/js/types/Decorators.js
@@ -0,0 +1,34 @@
+export function ValidaDebito(
+// Parametros para adicionar comportamento de Decorators:
+target, // Receber objeto com método decorado.
+propertyKey, // NomeDoMétodo.
+//   ------------------
+descriptor // Método Original.
+) {
+    // Guardar uma referência do método original recebido:
+    const metodoOriginal = descriptor.value;
+    // Substituir o método original por uma nova função:
+    descriptor.value = function (valorDoDebito) {
+        // lógica de validação (nesse caso, sendo para Validação do Débito)
+        if (valorDoDebito <= 0) {
+            throw new Error("O valor a ser debitado precisa ser maior que zero!");
+        }
+        if (valorDoDebito > this.saldo) {
+            throw new Error("Saldo insuficiente para realizar a operação!");
+        }
+        // Retorna o descriptor modificado:
+        return metodoOriginal.apply(this, [valorDoDebito]);
+    };
+    return descriptor;
+}
+// Repetição da mesma estrutura para validar o Deposito:
+export function ValidaDeposito(target, propertyKey, descriptor) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (valorDoDeposito) {
+        if (valorDoDeposito <= 0) {
+            throw new Error("O valor do deposito deve ser maior que zero!");
+        }
+        return metodoOriginal.apply(this, [valorDoDeposito]);
+    };
+    return descriptor;
+}
